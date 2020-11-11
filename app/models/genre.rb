5e040c22cb0e8de59c0124e53f819a92cc9f4fa8
Mikel Bishop <mdrew8@gmail.com>
2020-11-11 01:30:04 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists..all.name 
+    self.all.map do |a|
+      a.artists.name 
   end
 end
